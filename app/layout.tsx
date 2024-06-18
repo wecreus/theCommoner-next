334601a69592009b19627fb79ace4432bcce33ae
wecreus <wecreus@gmail.com>
2024-06-18 14:01:29 +0300

    feat: self hosting Inter font with specific slnt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Footer from "@/app/ui/footer/footer";
 import Header from "@/app/ui/header/header";
+import localFont from 'next/font/local';
 import "./globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+// https://stackoverflow.com/questions/78636740/how-to-specify-font-slant-slnt-when-using-nextjs
+const inter = localFont({ src: '../fonts/Inter.woff2' })
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://commoner.vercel.app/'),
+  metadataBase: new URL("https://commoner.vercel.app/"),
   title: "theCommoner",
   description: "Portfolio website made by Danylo Riabchuk",
 };
@@ -19,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} theme1`}>
+      <head>
+        {/* <link
+          rel="stylesheet"
+          href="//fonts.googleapis.com/css2?family=Inter:slnt,wght@-8,100..900&display=swap"
+        /> */}
+      </head>
+      <body className={`theme1 ${inter.className}`}>
         <Header />
         {children}
         <Footer />
