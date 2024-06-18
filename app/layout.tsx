017c9e069348bf767905743461e600bd84b4e43d
wecreus <wecreus@gmail.com>
2024-06-19 01:15:00 +0300

    fix: fonts on mobile displayed weirdly, loading fonts from google CDN

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,10 @@ import GradientSVG from "./ui/GradientSVG";
 import "@/styles/globals.scss";
 
 // https://stackoverflow.com/questions/78636740/how-to-specify-font-slant-slnt-when-using-nextjs
-const inter = localFont({ src: "../fonts/Inter.woff2" });
+
+// for some reason loading font from a local file results in a different font on mobile than on desktop
+// who knows why
+// const inter = localFont({ src: "../fonts/Inter.woff2" });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://commoner.vercel.app/"),
@@ -25,12 +28,8 @@ export default function RootLayout({
     <StoreProvider>
       <html lang="en">
         <head>
-          {/* <link
-          rel="stylesheet"
-          href="//fonts.googleapis.com/css2?family=Inter:slnt,wght@-8,100..900&display=swap"
-        /> */}
         </head>
-        <body className={inter.className}>
+        <body >
           <ThemeProvider>
             <Header />
             {children}
