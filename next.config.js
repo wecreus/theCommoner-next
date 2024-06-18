29a78ea31ea6bba1097b072b46752f1e8c93eca1
wecreus <wecreus@gmail.com>
2024-06-18 10:04:52 +0300

    config: turbopack config for svgr

--- next.config.js
+++ next.config.js
@@ -4,17 +4,18 @@
 const path = require("path");
 
 module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack']
-    });
-    return config;
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     additionalData: `@import "mixins"; @import "variables";`,
   },
 };
-
-
