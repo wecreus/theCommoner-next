d63479e3b9613c60dae6e9ffc31998cfe184e7e0
wecreus <wecreus@gmail.com>
2024-06-19 01:32:57 +0300

    feat: loading spinner from theCommoner

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import { ClipLoader } from "react-spinners";
+
+export default function Loading() {
+  return (
+    <div className="Spinner">
+      <ClipLoader color="white" size={45} speedMultiplier={0.5}/>
+    </div>
+  );
+}
