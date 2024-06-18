a01ff38f6bbddeddbc0fbd6f83ccbc5f609a06be
wecreus <wecreus@gmail.com>
2024-06-18 18:21:14 +0300

    feat: home boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,89 @@
-export default function Home() {
+'use client';
+
+import { useState, lazy, Suspense, useEffect, useMemo } from "react";
+// import Welcome from "./HomeComponents/Welcome";
+// import MainPageSelector from "./HomeComponents/MainPageSelector";
+// import useIntersectionObserver from "@/common/hooks/useIntersectionObserver";
+// import LoadingSpinner from "@/common/LoadingSpinner/LoadingSpinner";
+import "./Home.scss";
+
+// const Reviews = lazy(() => import("./HomeComponents/Reviews/Reviews"));
+// const Gallery = lazy(() => import("./HomeComponents/Gallery/Gallery"));
+// const Map = lazy(() => import("./HomeComponents/Map/Map"));
+
+const Home = () => {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  // const [galleryRef, isGalleryVisible, wasGalleryVisible] =
+  //   useIntersectionObserver();
+  // const [reviewsRef, isReviewsVisible, wasReviewsVisible] =
+  //   useIntersectionObserver();
+  // const [mapRef, isMapVisible, wasMapVisible] = useIntersectionObserver();
+  // const [welcomeRef, isWelcomeVisible] = useIntersectionObserver();
+
+  // // order of items in isVisibleList and in refList is important for scrolling
+  // const isVisibleList = useMemo(
+  //   () => [isWelcomeVisible, isReviewsVisible, isGalleryVisible, isMapVisible],
+  //   [isWelcomeVisible, isReviewsVisible, isGalleryVisible, isMapVisible]
+  // );
+
+  // const refList = [welcomeRef, reviewsRef, galleryRef, mapRef];
+
+  // const handlePageChange = (page) => {
+  //   refList[page].current.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "center",
+  //   });
+  // };
+
+  // // should update page number on scroll
+  // useEffect(() => {
+  //   for (let i = isVisibleList.length - 1; i >= 0; i--) {
+  //     if (isVisibleList[i]) {
+  //       setPageNumber(i);
+  //       return;
+  //     }
+  //   }
+  // }, [isVisibleList]);
+
   return (
-    <div >
-      theCommoner
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br /> sdjkf
-    </div>
+    <>
+      {/* <MainPageSelector
+        totalPages={refList.length}
+        currentPage={pageNumber}
+        handlePageChange={handlePageChange}
+      /> */}
+      <section className="card card-welcome">
+        welcome
+        {/* <Welcome onScrollClick={() => handlePageChange(1)} /> */}
+      </section>
+      <section className="card card-reviews">
+        {/* Reviews needs to know when user scrolls to it */}
+        {/*
+        {wasReviewsVisible && (
+          <Suspense fallback={<LoadingSpinner />}>
+            
+            <Reviews focused={isReviewsVisible} />
+          </Suspense>
+        )} 
+        */}
+      </section>
+      <section className="card card-gallery">
+        {/* {wasGalleryVisible && (
+          <Suspense fallback={<LoadingSpinner />}>
+            <Gallery />
+          </Suspense>
+        )} */}
+      </section>
+      <section className="card card-map">
+        {/* {wasMapVisible && (
+          <Suspense fallback={<LoadingSpinner />}>
+            <Map />
+          </Suspense>
+        )} */}
+      </section>
+    </>
   );
-}
+};
+
+export default Home;
