eb73d5b9eb1d643a55340c2b2a5ba6f5d65f4e3d
wecreus <wecreus@gmail.com>
2024-06-18 09:28:40 +0300

    feat: svgr support without viewbox

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 // @ts-check
- 
+
 /** @type {import('next').NextConfig} */
-const path = require('path');
+const path = require("path");
 
-
-const nextConfig = {
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack']
+    });
+    return config;
+  },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
     additionalData: `@import "mixins"; @import "variables";`,
   },
-}
- 
-module.exports = nextConfig
+};
+
+
