737adae33188563dcecd23b570ce99b9b319022b
wecreus <wecreus@gmail.com>
2024-06-20 03:43:18 +0300

    feat: basic map component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
 import Welcome from "@/app/ui/Welcome/Welcome";
-import Gallery from "@/app/ui/Gallery/Gallery";
-import Reviews from "@/app/ui/Reviews/Reviews";
+// import Gallery from "@/app/ui/Gallery/Gallery";
+// import Reviews from "@/app/ui/Reviews/Reviews";
 import Loading from "./loading";
 import { getReviews } from "@/app/lib/data";
 import { GalleryPictures } from "@/app/lib/data";
+import dynamic from "next/dynamic";
 import "./Home.scss";
 
-// const Gallery = dynamic(() => import("@/app/ui/gallery/Gallery"), {
-//   loading: () => <Loading />,
-// });
+const Gallery = dynamic(() => import("@/app/ui/Gallery/Gallery"), {
+  loading: () => <Loading />,
+});
 
-// const Reviews = dynamic(() => import("@/app/ui/reviews/Reviews"), {
-//   loading: () => <Loading />,
-// });
+const Reviews = dynamic(() => import("@/app/ui/Reviews/Reviews"), {
+  loading: () => <Loading />,
+});
+
+const Map = dynamic(() => import("@/app/ui/Map/Map"), {
+  loading: () => <Loading />,
+});
 
 const Home = async () => {
   const reviews = await getReviews();
@@ -30,6 +35,7 @@ const Home = async () => {
         <Gallery gallery={GalleryPictures} />
       </section>
       <section className="card card-map">
+        <Map />
         {/* {wasMapVisible && (
           <Suspense fallback={<LoadingSpinner />}>
             <Map />
---
+++ app/ui/Map/Map.tsx
@@ -0,0 +1,9 @@
+const Map = () => {
+  return (
+    <div className="map">
+      MAP
+    </div>
+  );
+};
+
+export default Map;
