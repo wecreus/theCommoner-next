bc80bcff943c51b78a32841395448816734a6e95
wecreus <wecreus@gmail.com>
2024-06-21 13:08:54 +0300

    feat: outsideClick popupHtml

--- app/ui/Map/PopupHTML.tsx
+++ app/ui/Map/PopupHTML.tsx
@@ -1,35 +1,52 @@
 import { Html } from "@react-three/drei";
-import { useRouter } from 'next/navigation'
-import { useAppSelector } from "@/app/lib/store/hooks";
+import { useRouter } from "next/navigation";
+import { updateIsInRange } from "@/app/lib/store/globe/globeSlice";
+import { useAppSelector, useAppDispatch } from "@/app/lib/store/hooks";
+import { useRef } from "react";
 
 const PopupHTML = ({ handleClick }: { handleClick: () => void }) => {
   const router = useRouter();
+  const outsideRef = useRef<HTMLDivElement>(null);
+  const dispatch = useAppDispatch();
   const isInRange = useAppSelector((store) => store.globe.isInRange);
 
+  const redirectHandler = () => {
+    router.push("/contact");
+    dispatch(
+      updateIsInRange({
+        isInRange: false,
+      })
+    );
+  };
+
+  const handleOutsideClick = (e: React.MouseEvent<HTMLElement>) => {
+    console.log(e.target);
+    if (e.target === outsideRef.current) {
+      handleClick();
+    }
+  };
+  
   return (
-    <>
-      <Html
-        position={[30.2, 53.5, 59.8]}
-        style={{
-          opacity: isInRange ? 1 : 0,
-          transform: "translate(0%, -50%)",
-        }}
-        className="three-embedded"
-        
-      >
-        <div className="three-embedded__content" onClick={handleClick}>
-          <span>
-            Currently based in <b>Ternopil</b>
-          </span>
-          <span
-            className="three-embedded__content--link"
-            onClick={() => router.push("/contact")}
-          >
-            contact me
-          </span>
-        </div>
-      </Html>
-    </>
+    <Html
+      position={[30.2, 53.5, 59.8]}
+      style={{
+        opacity: isInRange ? 1 : 0,
+        transform: "translate(0%, -50%)",
+      }}
+      className="three-embedded"
+    >
+      <div className="three-embedded__content" onClick={handleOutsideClick} ref={outsideRef}>
+        <span style={{ pointerEvents: "none" }}>
+          Currently based in <b>Ternopil</b>
+        </span>
+        <span
+          className="three-embedded__content--link"
+          onClick={redirectHandler}
+        >
+          contact me
+        </span>
+      </div>
+    </Html>
   );
 };
 
